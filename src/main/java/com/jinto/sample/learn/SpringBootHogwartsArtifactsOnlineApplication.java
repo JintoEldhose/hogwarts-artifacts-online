de5558e2766d0ca9628c09a467aabfae3881f1fc
Jinto Eldhose <jintoeldhose842@gmail.com>
2024-12-19 15:49:11 +0530

    Initial Commit

---
+++ src/main/java/com/jinto/sample/learn/SpringBootHogwartsArtifactsOnlineApplication.java
@@ -0,0 +1,14 @@
+package com.jinto.sample.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHogwartsArtifactsOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHogwartsArtifactsOnlineApplication.class, args);
+		System.out.println("Testing JINTO")
+	}
+
+}
---
+++ src/test/java/com/jinto/sample/learn/SpringBootHogwartsArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinto.sample.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHogwartsArtifactsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
